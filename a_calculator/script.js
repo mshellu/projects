99f1954d04c4c770b6fb15057e5af8cfd110b8fb
mshellu <70416548+mshellu@users.noreply.github.com>
2022-01-19 09:55:15 -0500

    Added CS50x folder

---
+++ a_calculator/script.js
@@ -0,0 +1,3 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+// document method querySelectorAll(): returns static NodeList (Node is an interface including Document)
+// static NodeList contains all elements 
